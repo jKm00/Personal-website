bd2d9abcfb18c544c5a5e77a45157259f72af203
jKm00 <joakied@stud.ntnu.no>
2022-07-12 21:19:06 +0200

    Removed console log

--- portfolio/src/routes/api/projects/index.ts
+++ portfolio/src/routes/api/projects/index.ts
@@ -3,8 +3,6 @@ import type { Project } from '../../../app/types/Project';
 import { projects } from '../../../store/projects';
 
 export const get: RequestHandler = (request: RequestEvent) => {
-	console.log(request.params);
-
 	return {
 		body: {
 			projects: projects
