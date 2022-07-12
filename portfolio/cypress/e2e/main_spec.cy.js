2e995e9a192bc50a09f9cfee80d866490a93f9cd
jKm00 <joakied@stud.ntnu.no>
2022-07-12 21:54:44 +0200

    Added test for api endpoints

--- portfolio/cypress/e2e/main_spec.cy.js
+++ portfolio/cypress/e2e/main_spec.cy.js
@@ -10,3 +10,21 @@ describe('UI tests', () => {
 		cy.url().should('include', '/portfolio');
 	});
 });
+
+describe('API test', () => {
+	it('Gets all projects', () => {
+		cy.request('/projects').as('projectsRequest');
+		cy.get('@projectsRequest').then((response) => {
+			expect(response.status).to.equal(200);
+			expect(response.body.projects.length).to.equal(6);
+		});
+	});
+
+	it('Get specific project', () => {
+		cy.request('/projects/1').as('projectRequest');
+		cy.get('@projectRequest').then((response) => {
+			expect(response.status).to.equal(200);
+			expect(response.body.project.id).to.equal(1);
+		});
+	});
+});
