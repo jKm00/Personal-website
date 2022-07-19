df0b5eb966d40d1d97f2940570d332ec854903bf
jKm00 <joakied@stud.ntnu.no>
2022-07-19 20:16:53 +0200

    Resume first draft

---
+++ portfolio/src/routes/resume.svelte
@@ -0,0 +1,265 @@
+<script lang="ts"></script>
+
+<section class="section">
+	<div class="content">
+		<section class="resume">
+			<!-- TODO: Considere if this should just be removed all together -->
+			<!-- <h1 class="resume__title title--primary title title--extra">
+				<span class="highlighted">Fullstack developer</span><br />
+				Joakim Edvardsen
+			</h1> -->
+			<header class="resume__header">
+				<p>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+						><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"
+						/></svg
+					>joakimedvardsen2000@gmail.com
+				</p>
+				<p>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+						><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M320 0H64C37.5 0 16 21.5 16 48v416C16 490.5 37.5 512 64 512h256c26.5 0 48-21.5 48-48v-416C368 21.5 346.5 0 320 0zM240 447.1C240 456.8 232.8 464 224 464H159.1C151.2 464 144 456.8 144 448S151.2 432 160 432h64C232.8 432 240 439.2 240 447.1z"
+						/></svg
+					>+47 901 59 363
+				</p>
+				<p>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+						><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z"
+						/></svg
+					>Ålesund, NO
+				</p>
+				<p>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+						><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+						/></svg
+					>linkedin.com/in/joakim-edvardsen
+				</p>
+			</header>
+			<!-- Section -->
+			<section class="resume__section">
+				<h2 class="resume__section__title title title--secondary">Work experience</h2>
+				<!-- Article -->
+				<article class="resume__section__card">
+					<h3 class="resume__section__card__title title">Goods Flow Co-worker</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">
+							21. Oktober 2019 - <br /> 31. Juli 2020
+						</p>
+						<p class="resume__section__card__stats__place">Trondheim</p>
+					</div>
+					<p class="resume__section__card__desc">
+						Worked at Ikea Leangen as a logistics and customer relations employee. Was hired as a
+						seasonal employee. I learned a lot about how a large store works in operation and how
+						each individual can contribute to creating good customer experiences.
+					</p>
+				</article>
+				<!-- Article -->
+				<article class="resume__section__card">
+					<h3 class="resume__section__card__title title">Part-time salesman at Yx / 7-eleven</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">10. Mai 2018 - <br /> 1. August 2019</p>
+						<p class="resume__section__card__stats__place">Vestby</p>
+					</div>
+					<p class="resume__section__card__desc">
+						Fisrt started as a summer aid and received training in May. Then continued as extra help
+						in the evenings and on weekends. I gained a lot of experience in the work environment,
+						but also learned a lot about how to treat customers and how a store / kiosk is run. At
+						the station there was a lot of varied work, which meant that new problems constantly
+						arose that had to be solved.
+					</p>
+				</article>
+				<!-- Article -->
+				<article class="resume__section__card">
+					<h3 class="resume__section__card__title title">Supervisor at ASP (SFO)</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">30. Mars 2016<br /> 3. Mai 2016</p>
+						<p class="resume__section__card__stats__place">Vestby</p>
+					</div>
+					<p class="resume__section__card__desc">
+						In 10th grade as part of my optional subject (technology), one day a week for five
+						weeks, we supervised children in coding. It was a great success as the children learnt a
+						lot, had a tun of fun and was very interested throughout.
+					</p>
+				</article>
+				<!-- Article -->
+				<article class="resume__section__card">
+					<h3 class="resume__section__card__title title">
+						Working week at the Norwegian Military Media Center
+					</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">
+							23. Februar 2015<br /> 28. Februar 2015
+						</p>
+						<p class="resume__section__card__stats__place">Oslo</p>
+					</div>
+					<p class="resume__section__card__desc">
+						In 9th grade, we had a work week where we were to be deployed in a place where we
+						wanted. I got the opportunity to work with the military's media center for a week. There
+						I got to see how they worked and got to participate in various activities such as
+						interviewing people on the street, see how their website was built and test out new
+						cameras.
+					</p>
+				</article>
+			</section>
+			<!-- Section -->
+			<section class="resume__section">
+				<h2 class="resume__section__title title title--secondary">Education</h2>
+				<!-- Article -->
+				<article class="resume__section__card resume__section__card--small-gap">
+					<h3 class="resume__section__card__title title">Computer science</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">
+							17. August 2020 -<br />
+						</p>
+						<p class="resume__section__card__stats__place">NTNU, Ålesund</p>
+					</div>
+					<p class="resume__section__card__desc">Bachelor’s degree in computer science</p>
+				</article>
+				<!-- Article -->
+				<article class="resume__section__card resume__section__card--small-gap">
+					<h3 class="resume__section__card__title title">Preperatory course for engineering</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">
+							19. August 2019 -<br /> 5. Juni 2020
+						</p>
+						<p class="resume__section__card__stats__place">NTNU, Trondheim</p>
+					</div>
+					<p class="resume__section__card__desc">
+						Mathematics and physics preperatory course for engineering
+					</p>
+				</article>
+				<!-- Article -->
+				<article class="resume__section__card resume__section__card--small-gap">
+					<h3 class="resume__section__card__title title">Highschool</h3>
+					<div class="resume__section__card__stats">
+						<p class="resume__section__card__stats__date">
+							9. August 2016 -<br /> 13. Juni 2019
+						</p>
+						<p class="resume__section__card__stats__place">Drømtorp, Ski</p>
+					</div>
+					<p class="resume__section__card__desc">
+						Media and communication with German as third language
+					</p>
+				</article>
+			</section>
+		</section>
+	</div>
+</section>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		justify-content: center;
+	}
+
+	.resume {
+		margin: 8rem 0;
+		font-family: var(--ff-body);
+		max-width: 55rem;
+
+		&__header {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			margin-bottom: 4rem;
+
+			& p {
+				display: flex;
+				align-items: center;
+				gap: 1rem;
+
+				& svg {
+					width: 1rem;
+					fill: var(--clr-accent-400);
+				}
+			}
+		}
+
+		&__section {
+			&__title {
+				margin-bottom: 1em;
+			}
+
+			&__card {
+				display: flex;
+				flex-direction: column;
+				gap: 1rem;
+
+				&__title {
+					color: var(--clr-accent-400);
+				}
+
+				&__stats {
+					display: flex;
+					justify-content: space-between;
+					align-items: end;
+					color: var(--clr-neutral-500);
+				}
+
+				&--small-gap {
+					row-gap: 0.5rem;
+				}
+			}
+
+			&__card:not(:last-child) {
+				margin-bottom: 3rem;
+			}
+		}
+
+		&__section:not(:last-child) {
+			margin-bottom: 5rem;
+		}
+	}
+
+	@media only screen and (min-width: 45rem) {
+		.resume {
+			margin: 10rem 0;
+
+			&__header {
+				display: grid;
+				grid-template-columns: repeat(2, 1fr);
+				justify-content: start;
+				margin-bottom: 5rem;
+			}
+		}
+	}
+
+	@media screen and (min-width: 55rem) {
+		.resume__section {
+			display: grid;
+			grid-template-columns: 10rem 1fr;
+			gap: 1rem;
+
+			&__title {
+				grid-column: 2 / span 1;
+				margin-bottom: 0.5rem;
+			}
+
+			&__card {
+				grid-column: 1 / -1;
+				display: grid;
+				grid-template-columns: inherit;
+				grid-template-areas:
+					'stats title'
+					'stats desc';
+
+				&__title {
+					grid-area: title;
+				}
+
+				&__stats {
+					grid-area: stats;
+					flex-direction: column;
+					align-items: start;
+				}
+
+				&__desc {
+					grid-area: desc;
+				}
+			}
+		}
+	}
+</style>
